6fae1cf7c60d2ef070ebb9146bdc5c7dd2c6b115
olabid212 <olabid212@gmail.com>
2024-06-04 15:59:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector ("#hamburger")
+const menu = document.querySelector ("#menu")
+
+hamburger.addEventListener("click", ()=>{
+menu.classList.toggle("hidden")
+})
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      "primarycolor": "#ED4137",
+      "secondarycolor": "#090619",
+      "textcolor": "#f1f1f1",
+    },
+    fontFamily: {
+      nunito: ["Nunito", 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
